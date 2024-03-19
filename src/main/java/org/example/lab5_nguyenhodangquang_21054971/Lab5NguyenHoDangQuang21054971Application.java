351ee1b8bbe4c3c27b66af32e88a3c62572bc802
Dang Quang <dangquang@duck.com>
2024-03-19 14:01:26 +0700

    'first

---
+++ src/main/java/org/example/lab5_nguyenhodangquang_21054971/Lab5NguyenHoDangQuang21054971Application.java
@@ -0,0 +1,13 @@
+package org.example.lab5_nguyenhodangquang_21054971;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab5NguyenHoDangQuang21054971Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab5NguyenHoDangQuang21054971Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/lab5_nguyenhodangquang_21054971/Lab5NguyenHoDangQuang21054971ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab5_nguyenhodangquang_21054971;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5NguyenHoDangQuang21054971ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
